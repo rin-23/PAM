905d7c05c97cde575775b24bb354eee015e64c2e
Rinat Abdrashitov <rindopuz23@gmail.com>
2013-10-29 14:09:49 -0400

    Added project files

---
+++ PAM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PAM
+//
+//  Created by Rinat Abdrashitov on 2013-10-29.
+//  Copyright (c) 2013 Rinat Abdrashitov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PAM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PAM
+//
+//  Created by Rinat Abdrashitov on 2013-10-29.
+//  Copyright (c) 2013 Rinat Abdrashitov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
